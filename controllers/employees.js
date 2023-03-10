6af1beb9a2eb88a6ad7984cef8baadd5076047d9
JONPIRES <jonbpires@gmail.com>
2023-03-10 15:45:08 -0300

    adjusting routes to index and new.ejs

--- controllers/employees.js
+++ controllers/employees.js
@@ -7,7 +7,7 @@ const {Employee} = require('../models')
 router.get('/', async(req,res,next)=>{
     try{
         let allEmp = await Employee.find({})
-        res.render('employee/index.ejs', {emp:allEmp})
+        res.render('employees/index.ejs', {emp:allEmp})
     }catch(err){
         console.log(err)
         return next()
@@ -16,11 +16,11 @@ router.get('/', async(req,res,next)=>{
 
 
 router.get('/new',(req,res,)=>{
-        res.render('employee/new.ejs')
+        res.render('employees/new.ejs')
 })
 
 
-router.Post('/new', async(req,res,next)=>{
+router.post('/new', async(req,res,next)=>{
     try{
         let newEmp = await Employee.create(req.body)
         res.redirect('/employee')
@@ -30,3 +30,4 @@ router.Post('/new', async(req,res,next)=>{
     }
 })
 
+module.exports = router
